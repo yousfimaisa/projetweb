cce0b75b0e859cdeeae24a42a2f30dae8f4b623d
maissa <maisayousfi095@gmail.com>
2025-05-10 00:29:27 +0100

    integration maissa

---
+++ view/FrontOffice/home.php
@@ -0,0 +1,23 @@
+<?php 
+include 'header.php';
+
+
+
+
+?>
+
+
+<div class="container mt-5">
+    <div class="text-center">
+        <h1>Welcome to Our Website!</h1>
+    </div>
+</div>
+
+
+
+<?php
+
+include 'footer.php';
+
+
+?>
