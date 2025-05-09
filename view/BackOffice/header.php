40e81ecc4dea1824a054bd2cbdb9543d16b89ef1
maissa <maisayousfi095@gmail.com>
2025-05-10 00:26:35 +0100

    integration maissa

---
+++ view/BackOffice/header.php
@@ -0,0 +1,104 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="utf-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <link rel="apple-touch-icon" sizes="76x76" href="assets/img/apple-icon.png">
+  <link rel="icon" type="image/png" href="assets/img/favicon.png">
+  <title>Soft UI Dashboard 3</title>
+<!-- Bootstrap 5 CSS -->
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-ENjdO4Dr2bkBIFxQpeo5piK9UJ0E6Q1dC2e1z9+t0odE2x0RvrKeOu0G4QF8U6Cf" crossorigin="anonymous">
+
+  <!-- Fonts and Icons -->
+  <link href="https://fonts.googleapis.com/css?family=Inter:300,400,500,600,700,800" rel="stylesheet" />
+  <link href="assets/css/nucleo-icons.css" rel="stylesheet" />
+  <link href="assets/css/nucleo-svg.css" rel="stylesheet" />
+  <head>
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
+    <!-- FontAwesome -->
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css" rel="stylesheet">
+	<link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" rel="stylesheet">
+
+</head>
+
+  <script src="https://kit.fontawesome.com/42d5adcbca.js" crossorigin="anonymous"></script>
+
+  <!-- CSS Files -->
+  <link id="pagestyle" href="assets/css/soft-ui-dashboard.css?v=1.1.0" rel="stylesheet" />
+</head>
+
+<body class="g-sidenav-show bg-gray-100">
+
+<!-- Sidebar -->
+<aside class="sidenav navbar navbar-vertical navbar-expand-xs border-0 border-radius-xl my-3 fixed-start ms-3" id="sidenav-main">
+  <div class="sidenav-header">
+    <i class="fas fa-times p-3 cursor-pointer text-secondary opacity-5 position-absolute end-0 top-0 d-xl-none" id="iconSidenav"></i>
+    <a class="navbar-brand m-0" href="#">
+      <img src="assets/img/logo-ct-dark.png" class="navbar-brand-img h-100" alt="main_logo">
+      <span class="ms-1 font-weight-bold">Soft UI Dashboard 3</span>
+    </a>
+  </div>
+  <hr class="horizontal dark mt-0">
+ <div class="collapse navbar-collapse w-auto" id="sidenav-collapse-main">
+    <ul class="navbar-nav">
+        <!-- Dashboard -->
+       <!-- Dashboard -->
+<li class="nav-item">
+    <a class="nav-link" href="stats.php">
+        
+		            <i class="fas fa-chart-line text-primary fa-2x"></i>
+
+        <span class="nav-link-text ms-1">Dashboard</span>
+    </a>
+</li>
+
+<!-- Gestion Paiements -->
+<li class="nav-item">
+    <a class="nav-link" href="back_pays.php">
+     
+                   <i class="fas fa-credit-card text-success fa-2x"></i>
+ <span class="nav-link-text ms-1">Gestion Paiements</span>
+    </a>
+</li>
+
+<li class="nav-item">
+    <a class="nav-link" href="back_user.php">
+     
+                   <i class="fas fa-user text-success fa-2x"></i>
+ <span class="nav-link-text ms-1">Gestion Utilisateurs</span>
+    </a>
+</li>
+
+<!-- Gestion Factures -->
+<li class="nav-item">
+    <a class="nav-link" href="back_factures.php">
+                    <i class="fas fa-file-invoice-dollar text-info fa-2x"></i>
+
+        <span class="nav-link-text ms-1">Gestion Factures</span>
+    </a>
+</li>
+<li class="nav-item">
+    <a class="nav-link" href="listrepondavis.php">
+        <i class="fas fa-comment-dots text-info fa-2x"></i>
+        <span class="nav-link-text ms-1">Gestion Avis</span>
+    </a>
+</li>
+
+    </ul>
+</div>
+
+
+
+
+<!-- Gestion promotion -->
+<li class="nav-item">
+    <a class="nav-link" href="http://localhost/VF/payment/view/Back_office/admin_dashboard.php">
+        <i class="fas fa-file-invoice-dollar text-info fa-2x"></i>
+        <span class="nav-link-text ms-1">Gestion Promotion</span>
+    </a>
+</li>
+
+</aside>
+
+<main class="main-content position-relative max-height-vh-100 h-100 border-radius-lg">
